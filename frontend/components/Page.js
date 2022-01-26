2c4d401836a734aa0e7017f65ebd00001447b4b6
AutoRooter <kryptonbot@gmail.com>
2022-01-26 09:46:22 +0100

    Frontend: Add Header and Nav

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,8 +1,11 @@
+import { Head } from 'next/document';
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children, cool }) {
   return (
     <div>
+      <Header />
       <h2> Page Component</h2>
       <h3>{cool}</h3>
       {children}
