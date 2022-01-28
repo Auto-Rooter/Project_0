c0e72934fb6b21e8469125cc4e8298c0c0756601
AutoRooter <kryptonbot@gmail.com>
2022-01-28 01:12:11 +0100

    Frontend: Server Side rendering Fix

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,17 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class Mydocument extends Document {
+  // NextJs Hook
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en-US">
