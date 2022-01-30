1575aa8ebbcf0f6c445575ee81a7b2865a744bcd
AutoRooter <kryptonbot@gmail.com>
2022-01-31 00:55:50 +0100

    Frontend: Adding custom react Form Hook

---
+++ frontend/lib/useForm.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  //   {
+  //       name: 'product 1',
+  //       description: 'nice product desc',
+  //       price: 1000
+  //   }
+
+  function handleChange(e) {
+    let { name, value, type } = e.target;
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankForm = Object.entries(inputs).map(([key, value]) => [key, '']);
+    setInputs(Object.fromEntries(blankForm));
+  }
+
+  return { inputs, handleChange, resetForm, clearForm };
+}
