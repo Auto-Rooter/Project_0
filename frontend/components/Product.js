bd1e6fa1c3858f76365d67dae5d3b657ca39bab4
AutoRooter <kryptonbot@gmail.com>
2022-01-30 14:38:08 +0100

    Frontend: products page (currency formatter) 2/2

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import ItemStyled from './styles/ItemStyles';
 import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
 
 export default function Product({ product }) {
   return (
@@ -13,7 +14,8 @@ export default function Product({ product }) {
       <Title>
         <Link href={`/product/${product.id}`}>{product.name}</Link>
       </Title>
-      <PriceTag>{product.price}</PriceTag>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
     </ItemStyled>
   );
 }
