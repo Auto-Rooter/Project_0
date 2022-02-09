6977a8c0a2710d1b31da24910bb8b97a4d7841aa
AutoRooter <kryptonbot@gmail.com>
2022-02-09 09:01:21 +0100

    Frontend: Product Update part 1

---
+++ frontend/components/UpdateProduct.js
@@ -0,0 +1,9 @@
+export default function UpdateProduct() {
+  // TODO
+  /*
+    1 - Get the Existing Product Info
+    2 - Get the Update Mutation 
+    3 - Build the updation Form
+    */
+  return <p>Update!</p>;
+}
