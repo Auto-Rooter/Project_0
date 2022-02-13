490115bcdef04d1d97f28a73dfaf9238d2ee38fc
AutoRooter <kryptonbot@gmail.com>
2022-02-13 09:22:53 +0100

    Frontend: Finish product Update

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,9 +1,121 @@
-export default function UpdateProduct() {
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Router from 'next/router';
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  // Get the Existing Product Info
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+
+  // Get the Update Mutation
+  const [
+    updateProduct,
+    { data: updateData, error: updateError, loading: updateLoading },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION, {
+    variables: {
+      id,
+    },
+  });
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: data?.Product?.image || '',
+    name: data?.Product?.name || '',
+    price: data?.Product?.price || 0.0,
+    description: data?.Product?.description || '',
+  });
   // TODO
   /*
-    1 - Get the Existing Product Info
+    1 - 
     2 - Get the Update Mutation 
     3 - Build the updation Form
     */
-  return <p>Update!</p>;
+  if (loading) return <p>Loading.....</p>;
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault(); // prevent from sending info to the server directly and putting the info in the URL
+        const res = await updateProduct({
+          variables: {
+            id,
+            name: inputs?.name || '',
+            price: inputs?.price || 0.0,
+            description: inputs?.description || '',
+          },
+        });
+      }}
+    >
+      <DisplayError error={error || updateError} />
+      <fieldset disabled={updateLoading} aria-busy={updateLoading}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Update Product</button>
+      </fieldset>
+    </Form>
+  );
 }
--- frontend/pages/update.js
+++ frontend/pages/update.js
@@ -1,9 +1,12 @@
+import { useRouter } from 'next/router';
 import UpdateProduct from '../components/UpdateProduct';
 
 export default function UpdatePage() {
+  const router = useRouter();
+
   return (
     <div>
-      <UpdateProduct />
+      <UpdateProduct id={router.query.id} />
     </div>
   );
 }
