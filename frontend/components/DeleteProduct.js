0c17fe8d744506dc7b3a98579a23be9e88672435
AutoRooter <kryptonbot@gmail.com>
2022-02-17 08:52:05 +0100

    Frontend: Delete Product functionality

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -1,3 +1,31 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
 export default function DeleteProduct({ id, children }) {
-  return <button type="button">{children}</button>;
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+  });
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure yo want to delete this item?')) {
+          console.log('DELETED');
+          deleteProduct().catch((e) => alert(e.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
 }
